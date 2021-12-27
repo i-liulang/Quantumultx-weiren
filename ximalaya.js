1af606548e03d6d7ef2d255944d490cf8e335976
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2021-12-28 01:37:52 +0800

    Update ximalaya.js

--- ximalaya.js
+++ ximalaya.js
@@ -4,7 +4,7 @@
 
 [rewrite_local]
 
-^http[s]?:\/\/.+ximalaya.+(mobile-user\/v2\/homePage|vip/v1/recommand/ts|business-vip-presale-core-web/product/info/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Quantumultx-weiren/main/ximalaya.js
+^http[s]?:\/\/.+ximalaya.+(mobile-user\/v2\/homePage|vip/v1/recommand/ts|business-vip-presale-core-web/product/info/ts|playpage/playpage/tabs).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Quantumultx-weiren/main/ximalaya.js
 
 [mitm]
 hostname= *xima*
@@ -14,9 +14,10 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const p1 = '/mobile-user/v2/homePage/';
+const p1 = '/mobile-user/v2/homePage';
 const p2 = '/vip/v1/recommand/ts';
 const p3 = '/business-vip-presale-core-web/product/info/ts';
+const p4 = 'playpage/playpage/tabs'
 //个人页面
 if (url.indexOf(p1) != -1) {
 obj.data.nickname = "伟人破解";
@@ -44,13 +45,17 @@ if (url.indexOf(p3) != -1) {
     obj.data.userInfo.userLogoPic = "https://cdn-upyun-images.dushu365.com/1634740120cf7b8f4e68d92f7c46696fc027c1681cp6ve9a";
     obj.data.userInfo.vipStatus = 2;
     obj.data.userInfo.expireDate = "2999-12-12";
-    obj.data.userInfo.userLevelIcon = "http://imagev2.xmcdn.com/group83/M01/0B/13/wKg5I18PvYPxfzF_AAARgN1Ol8Y902.png";
+    obj.data.userInfo.userLevel = 2;
      delete obj.data.recommendVipProduct;
      delete obj.data.moreVipChannels;
-    delete obj.data.jointVipProducts;
-   delete obj.data.vipProducts;
-
-delete obj.data.ads;
+     delete obj.data.jointVipProducts;
+     delete obj.data.vipProducts;
+     delete obj.data.ads;
+	body = JSON.stringify(obj);
+}
+//播放页面
+if (url.indexOf(p4) != -1) {
+	delete obj.data.promotionGuideInfo;
 	body = JSON.stringify(obj);
 }
 
