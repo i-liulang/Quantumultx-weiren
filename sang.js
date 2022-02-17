abccfab34d4b33cada9cafb7f3aa0c2c908c46ba
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-02-17 17:04:40 +0800

    Add files via upload

---
+++ sang.js
@@ -0,0 +1,27 @@
+/*
+
+
+[rewrite_local]
+
+#三更
+
+
+[mitm]
+hostname = *txt2021*,*kaqcn*
+
+*/
+
+
+
+var modifiedHeaders = $request.headers;
+
+modifiedHeaders['seq'] = 'AFD9C140-4C6E-469E-8F4D-535D9184699F';
+
+modifiedHeaders['token'] = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzE4OTUzODg5IiwiZXhwIjoxOTYwNDQ4NDM5fQ.neA7r8xkGWZ1A-N2KzyFhGkLWZg2dt3mu9lMT4QllqC9TBjHFnHUR_y-0dXwZ_7HHlRhzd52zSyeKMtpu5uicw';
+
+modifiedHeaders['User-Agent'] = 'FourInOne/3.5.3 (iPhone; iOS 14.7; Scale/2.00)';
+
+modifiedHeaders['deviceos'] = 'ios_iPhone9,1 ; iOS 14.7 ;';
+
+$done({headers : modifiedHeaders});
+
