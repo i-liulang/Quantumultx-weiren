12eb6af33a3514480ee3cfc3ff962d2213f8da3a
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-02-22 10:17:38 +0800

    Update 69mn.js

--- 69mn.js
+++ 69mn.js
@@ -13,10 +13,10 @@ hostname =
 
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['uid'] = '730987';
-modifiedHeaders['token'] = 'a0f1562058820e6af8efecb3fefa4820';
+modifiedHeaders['uid'] = '1295696';
+modifiedHeaders['token'] = '6c0c4ece67c760fb6c5a1bdbf7f0a2e8';
 modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1';
-modifiedHeaders['Cookie'] = 'avatar=https%3A%2F%2Fixbtxi.yechunyang.com%2Fmedia%2Fheadico%2F25.bnc%3Fext%3D.png%26_v%3D%26time%3D1643644800%26token%3D2e37e1ad13e6fc6739593edc26bd4047; bindCode=U774FL; nickname=%E4%BC%9F%E4%BA%BA; token=a0f1562058820e6af8efecb3fefa4820; uid=730987; username=7N94F5; channel=t69mn64';
+modifiedHeaders['Cookie'] = 'channel=t69mn64; nickname=%E4%BC%9F%E4%BA%BA; avatar=https%3A%2F%2Fixbtxi.yechunyang.com%2Fmedia%2Fheadico%2F8.bnc%3Fext%3D.png%26_v%3D%26time%3D1643644800%26token%3D2e37e1ad13e6fc6739593edc26bd4047; bindCode=null; token=6c0c4ece67c760fb6c5a1bdbf7f0a2e8; uid=1295696; username=3CMZVF';
 
 $done({headers : modifiedHeaders});
 
