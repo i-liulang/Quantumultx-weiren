4ddfaa102dd940445e29dd8a7567a6464c73f4d0
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-02-22 10:12:43 +0800

    Update 69luoli.js

--- 69luoli.js
+++ 69luoli.js
@@ -5,8 +5,7 @@
 
 #69萝莉解锁会员
 
-^http[s]?:\/\/h5x\.h.+\.com\/h5\/movie.*$ url script-request-header https://raw.githubusercontent.com/WeiRen0/Quantumultx-weiren/main/69luoli.js
-
+^https://h5.+com/h5/movie.*$ url script-request-header 69luoli.js
 [mitm]
 hostname = 
 
@@ -14,11 +13,10 @@ hostname =
 
 var modifiedHeaders = $request.headers;
 
-modifiedHeaders['uid'] = '4614548';
-modifiedHeaders['token'] = '3f45f4e1992befa0fdead016bda50c83';
+modifiedHeaders['uid'] = '6053637';
+modifiedHeaders['token'] = '5da7ac6d7ea999803c88bbc8513f8def';
 modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1';
-modifiedHeaders['Cookie'] = 'avatar=https%3A%2F%2Fixbtxi.yechunyang.com%2Fmedia%2Fheadico%2F21.bnc%3Fext%3D.png%26_v%3D%26time%3D1643644800%26token%3D2e37e1ad13e6fc6739593edc26bd4047; bindCode=null; nickname=%E4%BC%9F%E4%BA%BA';
+modifiedHeaders['Cookie'] = 'nickname=%E4%BC%9F%E4%BA%BA; avatar=https%3A%2F%2Fixbtxi.yechunyang.com%2Fmedia%2Fheadico%2F26.bnc%3Fext%3D.png%26_v%3D%26time%3D1643644800%26token%3D2e37e1ad13e6fc6739593edc26bd4047; bindCode=null; token=5da7ac6d7ea999803c88bbc8513f8def; uid=6053637; username=Q7V2SF';
 
 $done({headers : modifiedHeaders});
-
 
