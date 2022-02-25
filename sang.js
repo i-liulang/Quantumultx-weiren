64fe68daa6f7af2b419ca0aa37e66530d9ba4eb6
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-02-25 13:55:09 +0800

    Update sang.js

--- sang.js
+++ sang.js
@@ -12,16 +12,4 @@ hostname = *txt2021*,*kaqcn*,*invlock*
 */
 
 
-
-var modifiedHeaders = $request.headers;
-
-modifiedHeaders['seq'] = 'AFD9C140-4C6E-469E-8F4D-535D9184699F';
-
-modifiedHeaders['token'] = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzE4OTc2MzAxIiwiZXhwIjoxOTYwNDU5OTI4fQ.Ms6EfZmpSZR_AYtrM4bt00n2TrV0hoQXTZkIlbIsrtof1XlIVT2foqLn3mtkaS63U8KprYM0CVU0dJfhSZMXXQ';
-
-modifiedHeaders['User-Agent'] = 'FourInOne/3.5.3 (iPhone; iOS 14.7; Scale/2.00)';
-
-modifiedHeaders['deviceos'] = 'ios_iPhone9,1 ; iOS 14.7 ;';
-//$notify("伟哥提醒您三更会员注入成功");
-$done({headers : modifiedHeaders});
-
+var modifiedHeaders=$request['headers'];modifiedHeaders['seq']='AFD9C140-4C6E-469E-8F4D-535D9184699F';modifiedHeaders['token']='eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyXzE4OTc2MzAxIiwiZXhwIjoxOTYwNDU5OTI4fQ.Ms6EfZmpSZR_AYtrM4bt00n2TrV0hoQXTZkIlbIsrtof1XlIVT2foqLn3mtkaS63U8KprYM0CVU0dJfhSZMXXQ';modifiedHeaders['User-Agent']='FourInOne/3.5.3\x20(iPhone;\x20iOS\x2014.7;\x20Scale/2.00)';modifiedHeaders['deviceos']='ios_iPhone9,1\x20;\x20iOS\x2014.7\x20;';$done({'headers':modifiedHeaders});
